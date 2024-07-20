a02cdae0ae9d93c07b5a29eda9a4f9edfd7f3386
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-07-20 13:49:56 -0700

    made ArithmeticDCHolder

---
+++ src/Main/ArithmeticDC/ArithmeticDCHolder.java
@@ -0,0 +1,12 @@
+package ArithmeticDC;
+
+import DataClass.*;
+
+public class ArithmeticDCHolder extends DCHolder {
+  public final AddSubtractDC addSubtractDC = new AddSubtractDC(2);
+
+  public ArithmeticDCHolder() {
+    
+  }
+
+}
