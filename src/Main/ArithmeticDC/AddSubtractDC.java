3d9383695299cd7833f5a9bd0f6ab544296b94b2
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-07-20 13:47:26 -0700

    created AddSubtractDC as a cdc, importing all required methods from said cdc

--- src/Main/ArithmeticDC/AddSubtractDC.java
+++ src/Main/ArithmeticDC/AddSubtractDC.java
@@ -1,4 +1,31 @@
 package ArithmeticDC;
 
-public class AddSubtractDC {
+import DataClass.*;
+
+public class AddSubtractDC extends CompoundDataClass {
+
+  public AddSubtractDC(int minimumRequiredSetValues) {
+    super(minimumRequiredSetValues);
+  }
+
+  @Override
+  public DataClassBrick makeBrick() {
+    return null;
+  }
+
+  @Override
+  public Result<DataClassBrick> calcInternal(String name, CompoundDataClassBrick outerAsBrick) {
+    return null;
+  }
+
+  @Override
+  public ConflictsCheckResult conflictsCheck(CompoundDataClassBrick brick, String targetName, Object targetVal) {
+    return null;
+  }
+
+  @Override
+  public DataClassBrick makeBrick(String name, CompoundDataClassBrick outer) {
+    return null;
+  }
+
 }
