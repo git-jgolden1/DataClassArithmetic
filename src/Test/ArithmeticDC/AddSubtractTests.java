66d9a147ee98e0d8c021678fe5d28720d92ebc2a
Jonathan Gurr <jonathan.golden.gurr@icloud.com>
2024-07-20 15:21:50 -0700

    added assertions to ensure that addSubtractDCB is or isn't complete at the appropriate places

--- src/Test/ArithmeticDC/AddSubtractTests.java
+++ src/Test/ArithmeticDC/AddSubtractTests.java
@@ -33,31 +33,37 @@ public class AddSubtractTests {
     Result r = addOp1DCB.putSafe(1);
     assertNull(r.getError());
     assertEquals(1, addOp1DCB.getVal());
+    assertFalse(addSubtractDCB.isComplete());
 
     //2 = addOp2 can also be putSafe if no sum is set
     r = addOp2DCB.putSafe(2);
     assertNull(r.getError());
     assertEquals(2, addOp2DCB.getVal());
+    assertTrue(addSubtractDCB.isComplete());
 
     //3 = sum can be putSafe if no conflicts exist with addOps
     r = sumDCB.putSafe(3);
     assertNull(r.getError());
     assertEquals(3, sumDCB.getVal());
+    assertTrue(addSubtractDCB.isComplete());
 
     //4 = sum CAN'T be putSafe if conflicts DO exist with addOps
     r = sumDCB.putSafe(4);
     assertNotNull(r.getError());
     assertEquals(3, sumDCB.getVal());
+    assertTrue(addSubtractDCB.isComplete());
 
     //5 = op1 CAN'T be putSafe if conflicts DO exist with addOp2 and sum
     r = addOp1DCB.putSafe(0);
     assertNotNull(r.getError());
     assertEquals(1, addOp1DCB.getVal());
+    assertTrue(addSubtractDCB.isComplete());
 
     //6 = op2 CAN'T be putSafe if conflicts DO exist with addOp1 and sum
     r = addOp2DCB.putSafe(5);
     assertNotNull(r.getError());
     assertEquals(2, addOp2DCB.getVal());
+    assertTrue(addSubtractDCB.isComplete());
 
   }
 
